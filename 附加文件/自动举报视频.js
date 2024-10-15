2951c9e10e64db58f6c14944cb64a6ae239b9aa4
Ayyayyayy2002 <joshualovejoy@outlook.com>
2024-10-15 13:15:36 +0800

    Update 自动举报视频.js

--- 附加文件/自动举报视频.js
+++ 附加文件/自动举报视频.js
@@ -96,7 +96,7 @@ function updateDiagnosticInfo(content) {
   scrollToBottom();
 }
 
-let delayInMilliseconds = 2400; // 设置延迟时间
+let delayInMilliseconds = 100; // 设置延迟时间
 
 // 更新 sendReportRequest 函数，在显示完最后一次举报返回值后显示“本页全部举报完成”，然后调用 clickPreviousPageButton 函数
 function sendReportRequest() {
