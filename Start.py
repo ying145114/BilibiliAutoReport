d9424525e32b2010c94376dea48c22f4f1046937
Ayyayyayy2002 <joshualovejoy@outlook.com>
2024-10-23 10:56:24 +0800

    Update Start.py

--- Start.py
+++ Start.py
@@ -3,7 +3,6 @@ import subprocess
 import os
 
 
-
 print('启动Getuid.py')
 base_dir = os.path.dirname(os.path.abspath(__file__))
 log_folder = os.path.join(base_dir)  # 确保目录存在
@@ -12,7 +11,6 @@ log_file_path = os.path.join(log_folder, '错误记录.txt')
 python_executable = os.path.join(base_dir, 'venv', 'Scripts', 'python.exe')
 getuid_script = os.path.join(base_dir, 'Getuid.py')
 report_script = os.path.join(base_dir, 'Report.py')
-
 
 
 def log_error(message):
@@ -26,8 +24,8 @@ while True:
         getuid_process = subprocess.Popen([python_executable, getuid_script], shell=True)
         getuid_process.wait()  # 等待 Getuid.py 结束
 
-        # 检查 Getuid.py 的退出状态
-        if getuid_process.returncode == 0:
+        
+        if getuid_process.returncode == 0:# 检查 Getuid.py 的退出状态
             print("Getuid.py 正常退出，正在启动 Report.py...")
             break  # 退出此循环，开始启动 Report.py
         else:
@@ -35,12 +33,13 @@ while True:
             print(error_message)
             log_error(error_message)  # 记录错误信息
 
+
     while True:  # 死循环以重启 Report.py
         report_process = subprocess.Popen([python_executable, report_script], shell=True)
         report_process.wait()  # 等待 Report.py 结束
 
-        # 检查 Report.py 的退出状态
-        if report_process.returncode == 0:
+       
+        if report_process.returncode == 0: # 检查 Report.py 的退出状态
             print("Report.py 正常退出，正在重新启动 Getuid.py...")
             break  # 退出此循环，重新开始下一轮
         else:
