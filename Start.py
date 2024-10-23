9c82979f02ccb90c61e2d602a8c5baf409fa5410
Ayyayyayy2002 <joshualovejoy@outlook.com>
2024-10-23 10:02:37 +0800

    Update Start.py

--- Start.py
+++ Start.py
@@ -2,27 +2,53 @@ import subprocess
 import sys
 import time
 import os
+from datetime import datetime
 
-print('所有关键词搜索和处理完成后，通过虚拟机在指定路径运行 Getuid.py')
+print('启动Getuid.py')
+
 base_dir = os.path.dirname(os.path.abspath(__file__))
-
+log_folder = os.path.join(base_dir)  # 确保目录存在
+os.makedirs(log_folder, exist_ok=True)
+log_file_path = os.path.join(log_folder, 'log.txt')
 # 使用相对路径设置 Python 可执行文件和脚本路径
+python_executable = os.path.join(base_dir, 'venv', 'Scripts', 'python.exe')
+getuid_script = os.path.join(base_dir, 'Getuid.py')
 report_script = os.path.join(base_dir, 'Report.py')
 
-while True:  # 无限循环以重启 Getuid.py
+# 日志文件路径
+
+
+def log_error(message):
+    """记录错误信息到日志文件"""
+    with open(log_file_path, 'a', encoding='utf-8') as log_file:
+        timestamp = datetime.now().strftime('[%Y-%m-%d %H:%M:%S]')
+        log_file.write(f"\n\n{timestamp} {message}")
+
+while True:  # 无限循环
     # 启动 Getuid.py
-    python_executable = os.path.join(base_dir, 'venv', 'Scripts', 'python.exe')
-    getuid_script = os.path.join(base_dir, 'Getuid.py')
+    while True:  # 死循环以重启 Getuid.py
+        getuid_process = subprocess.Popen([python_executable, getuid_script], shell=True)
+        getuid_process.wait()  # 等待 Getuid.py 结束
 
-    # 启动子进程
-    report_process = subprocess.Popen([python_executable, getuid_script], shell=True)
-    while report_process.poll() is None:  # 等待 report.py 结束
-        time.sleep(1)  # 等待1秒钟
+        # 检查 Getuid.py 的退出状态
+        if getuid_process.returncode == 0:
+            print("Getuid.py 正常退出，正在启动 Report.py...")
+            break  # 退出此循环，开始启动 Report.py
+        else:
+            error_message = f"Getuid.py 出现错误，返回码: {getuid_process.returncode}，正在重新运行 Getuid.py..."
+            print(error_message)
+            log_error(error_message)  # 记录错误信息
 
-    # 检查 report.py 的退出状态
-    if report_process.returncode == 0:
-        print("Getuid.py 正常退出，正在重新启动 Getuid.py...")
-        subprocess.run([sys.executable, __file__])  # 重新启动 getuid.py
-        break  # 跳出循环，防止再次进入无限循环
-    else:
-        print(f"Getuid.py 出现错误，返回码: {report_process.returncode}，正在重新运行 Getuid.py...")
+    # 启动 Report.py
+    while True:  # 死循环以重启 Report.py
+        report_process = subprocess.Popen([python_executable, report_script], shell=True)
+        report_process.wait()  # 等待 Report.py 结束
+
+        # 检查 Report.py 的退出状态
+        if report_process.returncode == 0:
+            print("Report.py 正常退出，正在重新启动 Getuid.py...")
+            break  # 退出此循环，重新开始下一轮
+        else:
+            error_message = f"Report.py 出现错误，返回码: {report_process.returncode}，正在重新运行 Report.py..."
+            print(error_message)
+            log_error(error_message)  # 记录错误信息
