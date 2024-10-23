5bc5c614a7d141b02abd2fac1d60acf30dc30c8f
ayyayyayy2002 <joshualovejoy@outlook.com>
2024-10-23 20:03:19 +0800

    Update Report.py

--- Report.py
+++ Report.py
@@ -112,7 +112,7 @@ if not uids:
 
 
 options = webdriver.ChromeOptions()
-#options.add_argument('--enable-logging')  # 启用控制台日志
+options.add_argument('--enable-logging')  # 启用控制台日志
 options.add_argument("--disable-blink-features=AutomationControlled")
 options.add_argument(f'--user-data-dir={user_data_dir}')  # 设置用户数据目录
 options.binary_location = chrome_binary_path  # 指定 Chrome 浏览器的可执行文件路径
@@ -120,8 +120,7 @@ options.add_argument('--proxy-server="direct://"')
 options.add_argument('--proxy-bypass-list=*')
 options.add_argument("--disable-gpu")
 options.add_argument("--disable-sync")
-#options.add_argument("--headless")
-options.add_argument("--headless=old")
+options.add_argument("--headless")
 service = Service(executable_path=chrome_driver_path)
 driver = webdriver.Chrome(service=service, options=options)# 启动 Chrome 浏览器
 #driver.set_window_size(1000, 700)  # 设置浏览器窗口大小（宽度, 高度）
