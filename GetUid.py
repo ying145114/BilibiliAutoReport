5ef98b820fd10463bd2e77cf22a3f73141941afd
Ayyayyayy2002 <joshualovejoy@outlook.com>
2024-10-23 10:54:11 +0800

    Update GetUid.py

--- GetUid.py
+++ GetUid.py
@@ -1,22 +1,27 @@
-import os
-import shutil
-import sys
-import time
-import subprocess
 from urllib.parse import urlencode
-import requests
 from bs4 import BeautifulSoup
-
-proxies = {
-    'http': None,
-    'https': None
-}
+import requests
+import shutil
+import os
 
 
-# 定义获取关键词的函数
-def fetch_keywords():
+proxies = {'http': None,'https': None}
+output_file = os.path.join(os.getcwd(), '附加文件/uid.txt')
+whitelist_url = 'https://raw.kkgithub.com/ayyayyayy2002/BiliBiliVideoAutoReport/main/云端文件/whitelist.txt'
+whitelist_filename = '附加文件/whitelist.txt'
+blacklist_url = 'https://raw.kkgithub.com/ayyayyayy2002/BiliBiliVideoAutoReport/main/云端文件/blacklist.txt'
+blacklist_filename = '附加文件/blacklist.txt'
+if os.path.exists(output_file):
+    os.remove(output_file)
+else:
+    print(f"文件 {output_file} 不存在，无需删除。")
+
+
+
+def fetch_keywords():# 定义获取关键词的函数
     keywords_url = 'https://raw.kkgithub.com/ayyayyayy2002/BiliBiliVideoAutoReport/main/云端文件/keyword.txt'  # 替换为实际的GitHub URL
     keywords_filename = '附加文件/keyword.txt'
+
 
     try:
         response = requests.get(keywords_url, proxies=proxies, timeout=(5, 10))
@@ -34,8 +39,7 @@ def fetch_keywords():
     return load_local_keywords(keywords_filename)
 
 
-# 定义从本地文件加载关键词的函数
-def load_local_keywords(filename):
+def load_local_keywords(filename):# 定义从本地文件加载关键词的函数
     keywords = []
     if os.path.exists(filename):
         with open(filename, 'r', encoding='utf-8') as f:
@@ -49,23 +53,7 @@ def load_local_keywords(filename):
     return keywords
 
 
-# 使用fetch_keywords函数替代原有的keywords定义
-keywords = fetch_keywords()
-
-# 文件路径和文件名
-output_file = os.path.join(os.getcwd(), '附加文件/uid.txt')
-whitelist_file = os.path.join(os.getcwd(), '附加文件/whitelist.txt')
-
-# 如果文件存在则删除
-if os.path.exists(output_file):
-    os.remove(output_file)
-else:
-    print(f"文件 {output_file} 不存在，无需删除。")
-
-
-# 定义搜索函数
-def search_and_extract_uid(keyword):
-    # 构建搜索链接
+def search_and_extract_uid(keyword):# 定义搜索函数
     base_url = 'https://search.bilibili.com/video?'
     search_params_list = [
         {
@@ -82,13 +70,14 @@ def search_and_extract_uid(keyword):
             'keyword': keyword,
             'from_source': 'video_tag',
             'order': 'pubdate'
-        },
-
+        }
     ]
+
 
     for search_params in search_params_list:
         search_url = base_url + urlencode(search_params)
         print(search_url)
+
 
         try:
             # 添加头部信息
@@ -96,18 +85,10 @@ def search_and_extract_uid(keyword):
                 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
                               'Chrome/91.0.4472.124 Safari/537.36',
             }
-
-            # 发起HTTP GET请求获取搜索结果页面内容
             response = requests.get(search_url, headers=headers, proxies=proxies, timeout=(5, 10))
             response.raise_for_status()  # 检查请求是否成功
-
-            # 使用BeautifulSoup加载HTML内容
             soup = BeautifulSoup(response.text, 'html.parser')
-
-            # 存储解析出的UID列表
             uid_list = []
-
-            # 使用CSS选择器定位搜索结果的链接，并提取UID
             count = 0  # 计数器，用于限制获取的UID数量
             for link in soup.select('.bili-video-card .bili-video-card__info--owner'):
                 if count >= 10:
@@ -116,16 +97,12 @@ def search_and_extract_uid(keyword):
                 uid = href.split('/')[-1]  # 获取链接中最后的数字部分作为UID
                 uid_list.append(uid)
                 count += 1
-
-            # 将UID列表传递给处理函数（这里假设是process_uid_list函数）
             process_uid_list(keyword, uid_list)
-
         except requests.exceptions.RequestException as e:
             print(f"关键词 \"{keyword}\" 搜索页面请求失败：", e)
 
 
-# 定义处理UID列表的函数（追加写入同一文件）
-def process_uid_list(keyword, uid_list):
+def process_uid_list(keyword, uid_list):# 定义处理UID列表的函数（追加写入同一文件）
     print(f" \"{keyword}\" UID：", uid_list)
 
     # 将UID列表追加写入文件
@@ -136,17 +113,14 @@ def process_uid_list(keyword, uid_list):
         f.write('\n')  # 添加空行分隔每个关键词的UID列表
 
 
-
-
-# 主函数，循环运行搜索和处理
-def main():
+def main():# 主函数，循环运行搜索和处理
     while True:
         unique_uids = set()  # 使用集合存储唯一的 UID
+        keywords = fetch_keywords()  # 使用fetch_keywords函数替代原有的keywords定义
 
-        # 遍历关键词列表，进行搜索和处理
-        for keyword in keywords:
+
+        for keyword in keywords:# 遍历关键词列表，进行搜索和处理
             search_and_extract_uid(keyword)
-
         print('读取当前文件中所有的 UID，并添加到集合中去重')
         with open(output_file, 'r', encoding='utf-8') as f:
             lines = f.readlines()
@@ -154,14 +128,15 @@ def main():
                 uid = line.strip()
                 if uid.isdigit():  # 假设 UID 是数字格式
                     unique_uids.add(uid)
+
+
         try:
             shutil.copy('附加文件/uid.txt', '附加文件/uid_backup.txt')
             print(f"成功保存备份")
         except IOError as e:
             print(f"复制保存备份时发生错误：{e}")
 
-        whitelist_url = 'https://raw.kkgithub.com/ayyayyayy2002/BiliBiliVideoAutoReport/main/云端文件/whitelist.txt'
-        whitelist_filename = '附加文件/whitelist.txt'
+
         try:
             response = requests.get(whitelist_url, proxies=proxies, timeout=(5, 10))
             if response.status_code == 200:
@@ -176,20 +151,18 @@ def main():
             print(f"文件操作发生错误：{e}")
         except Exception as e:
             print(f"发生未知错误：{e}")
-        # 处理 whitelist_file
-        with open(whitelist_file, 'r', encoding='utf-8') as f:
+
+
+        with open(whitelist_filename, 'r', encoding='utf-8') as f:# 处理 whitelist_file
             lines = f.readlines()
             for line in lines:
                 uid = line.strip()
                 if uid.isdigit():  # 假设 UID 是数字格式
                     unique_uids.add(uid)
 
-        blacklist_url = 'https://raw.kkgithub.com/ayyayyayy2002/BiliBiliVideoAutoReport/main/云端文件/blacklist.txt'
-        blacklist_filename = '附加文件/blacklist.txt'
 
         try:
             response = requests.get(blacklist_url, proxies=proxies, timeout=(5, 10))
-
             if response.status_code == 200:
                 with open(blacklist_filename, 'wb') as f_out:
                     f_out.write(response.content)
@@ -202,24 +175,24 @@ def main():
             print(f"文件操作发生错误：{e}")
         except Exception as e:
             print(f"发生未知错误：{e}")
-
         print('继续执行其他操作，不受文件下载错误的影响')
         exclude_uids = set()
-        with open('附加文件/blacklist.txt', 'r', encoding='utf-8') as exclude_file:
+
+
+        with open(blacklist_filename, 'r', encoding='utf-8') as exclude_file:
             exclude_lines = exclude_file.readlines()
             for line in exclude_lines:
                 exclude_uid = line.strip()
                 if exclude_uid.isdigit():  # 假设 UID 是数字格式
                     exclude_uids.add(exclude_uid)
-
         print('从 unique_uids 中移除在 exclude_uids 中存在的 UID')
         unique_uids -= exclude_uids
-
         print('将唯一的 UID 列表按格式写入文件')
+
+
         with open(output_file, 'w', encoding='utf-8') as f:
             for uid in unique_uids:
                 f.write(uid + '\n')
-
         print('关键词搜索和UID全部处理完成')
         exit(0)
 
