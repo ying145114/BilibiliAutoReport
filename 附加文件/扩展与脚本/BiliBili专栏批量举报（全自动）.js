76cb484d0b568ac5da216c4345a00459dd8cd37a
Ayyayyayy2002 <joshualovejoy@outlook.com>
2024-10-23 16:24:56 +0800

    Update BiliBili专栏批量举报（全自动）.js

--- 附加文件/扩展与脚本/BiliBili专栏批量举报（全自动）.js
+++ 附加文件/扩展与脚本/BiliBili专栏批量举报（全自动）.js
@@ -5,6 +5,8 @@
 // @description  DEMO
 // @author       Your Name
 // @match        https://space.bilibili.com/*/article
+// @exclude      https://space.bilibili.com/*/dynamic
+// @exclude      https://space.bilibili.com/*/video
 // @grant        GM_xmlhttpRequest
 // @connect      api.bilibili.com
 // @icon         https://i2.hdslb.com/bfs/app/8920e6741fc2808cce5b81bc27abdbda291655d3.png@240w_240h_1c_1s_!web-avatar-space-header.avif
