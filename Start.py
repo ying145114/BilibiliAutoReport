d4cc5f1165bcb6b33e3c4bd81ec7ac7c58cb3901
Ayyayyayy2002 <joshualovejoy@outlook.com>
2024-10-23 10:55:36 +0800

    Update Start.py

--- Start.py
+++ Start.py
@@ -1,31 +1,27 @@
-import subprocess
-import sys
-import time
-import os
 from datetime import datetime
+import subprocess
+import os
+
+
 
 print('启动Getuid.py')
-
 base_dir = os.path.dirname(os.path.abspath(__file__))
 log_folder = os.path.join(base_dir)  # 确保目录存在
 os.makedirs(log_folder, exist_ok=True)
-log_file_path = os.path.join(log_folder, 'log.txt')
-# 使用相对路径设置 Python 可执行文件和脚本路径
+log_file_path = os.path.join(log_folder, '错误记录.txt')
 python_executable = os.path.join(base_dir, 'venv', 'Scripts', 'python.exe')
 getuid_script = os.path.join(base_dir, 'Getuid.py')
 report_script = os.path.join(base_dir, 'Report.py')
 
-# 日志文件路径
 
 
 def log_error(message):
-    """记录错误信息到日志文件"""
     with open(log_file_path, 'a', encoding='utf-8') as log_file:
         timestamp = datetime.now().strftime('[%Y-%m-%d %H:%M:%S]')
         log_file.write(f"\n\n{timestamp} {message}")
 
-while True:  # 无限循环
-    # 启动 Getuid.py
+
+while True:
     while True:  # 死循环以重启 Getuid.py
         getuid_process = subprocess.Popen([python_executable, getuid_script], shell=True)
         getuid_process.wait()  # 等待 Getuid.py 结束
@@ -39,7 +35,6 @@ while True:  # 无限循环
             print(error_message)
             log_error(error_message)  # 记录错误信息
 
-    # 启动 Report.py
     while True:  # 死循环以重启 Report.py
         report_process = subprocess.Popen([python_executable, report_script], shell=True)
         report_process.wait()  # 等待 Report.py 结束
