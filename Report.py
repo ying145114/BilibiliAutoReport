301db8e401f752220e66a3bf18875eb2e0df5d19
Ayyayyayy2002 <joshualovejoy@outlook.com>
2024-10-25 15:45:49 +0800

    Update Report.py

--- Report.py
+++ Report.py
@@ -128,7 +128,7 @@ options.add_argument('--proxy-server="direct://"')
 options.add_argument('--proxy-bypass-list=*')
 options.add_argument("--disable-gpu")
 options.add_argument("--disable-sync")
-#options.add_argument("--headless")
+options.add_argument("--headless")
 options.add_argument('log-level=3')
 service = Service(executable_path=chrome_driver_path)
 driver = webdriver.Chrome(service=service, options=options)# 启动 Chrome 浏览器
