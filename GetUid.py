799030d60e74f5581842d88701d02b90d0751e5c
ayyayyayy2002 <joshualovejoy@outlook.com>
2024-10-27 23:11:36 +0800

    Update GetUid.py

--- GetUid.py
+++ GetUid.py
@@ -137,24 +137,6 @@ def search_and_extract_uid(keyword):  # 定义搜索函数
         {
             'keyword': keyword,
             'from_source': 'video_tag',
-            'page': '3',
-            'o': '60'
-        },
-        {
-            'keyword': keyword,
-            'from_source': 'video_tag',
-            'page': '4',
-            'o': '90'
-        },
-        {
-            'keyword': keyword,
-            'from_source': 'video_tag',
-            'page': '5',
-            'o': '120'
-        },
-        {
-            'keyword': keyword,
-            'from_source': 'video_tag',
             'order': 'pubdate'
         }
     ]
