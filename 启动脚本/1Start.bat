74d305b2ab839d14e7562ed7b67fc6b20476e63c
Ayyayyayy2002 <joshualovejoy@outlook.com>
2024-10-30 11:45:50 +0800

    更新

--- 启动脚本/1Start.bat
+++ 启动脚本/1Start.bat
@@ -1,7 +1,9 @@
 =@echo off
 chcp 65001
 setlocal
+pushd ..
 call venv\Scripts\activate.bat
 python Start.py
+popd
 pause
 endlocal
--- 启动脚本/2GetUid.bat
+++ 启动脚本/2GetUid.bat
@@ -1,14 +1,9 @@
 =@echo off
 chcp 65001
 setlocal
-
-:: 激活虚拟环境
+pushd ..
 call venv\Scripts\activate.bat
-
-:: 运行 Start.py
 python GetUid.py
-
-:: 可选：停留在命令行窗口，查看输出
+popd
 pause
-
 endlocal
