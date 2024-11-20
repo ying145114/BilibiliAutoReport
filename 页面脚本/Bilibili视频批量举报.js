e63450118ca08e6fbb949e615838c329c93099af
ayyayyayy2002 <joshualovejoy@outlook.com>
2024-11-21 07:19:44 +0800

    .

--- 页面脚本/Bilibili视频批量举报.js
+++ 页面脚本/Bilibili视频批量举报.js
@@ -2,7 +2,7 @@ var callback = arguments[arguments.length - 1];
 
 let reportCount = 0
 let currentAidIndex = 0; // 当前处理的AID索引
-let time_video = 2200
+let time_video = 2300
 let aids = []; // 所有提取的AID
 const floatingWindow = document.createElement('div');// 创建诊断信息窗口
 floatingWindow.style.position = 'fixed';
