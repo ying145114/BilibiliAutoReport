b127627be9522b8d90ff0cee4601187ee25d5362
Ayyayyayy2002 <joshualovejoy@outlook.com>
2024-11-26 16:58:34 +0800

    Update Report.py

--- Report.py
+++ Report.py
@@ -108,7 +108,7 @@ try:
     for uid in uids:
         try:
 
-            search_url = f'https://api.bilibili.com/x/series/recArchivesByKeywords?mid={uid}&keywords=&ps=1&orderby=views'
+            search_url = f'https://api.bilibili.com/x/series/recArchivesByKeywords?mid={uid}&keywords=&ps=1'
             headers = {
                 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
             response = requests.get(search_url, headers=headers, proxies=proxies, timeout=(5, 10))
