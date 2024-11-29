08440e6cdd5df568930101d4fb7e85e6d0149ab7
Ayyayyayy2002 <joshualovejoy@outlook.com>
2024-11-29 17:08:47 +0800

    Update 总脚本.js

--- 附加文件/页面脚本/总脚本.js
+++ 附加文件/页面脚本/总脚本.js
@@ -323,7 +323,7 @@ function submitAppeal(aid, title, pic) {
 
 
 
-                if (reportCount === 20){
+                if (reportCount === 1){
                 callback(output);
                 }
 
